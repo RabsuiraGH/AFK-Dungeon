5cb82830d4159b6a2a18eca4549cca697a39d448
RabsuiraGH <rabsuira@proton.me>
2025-09-15 01:51:06 +0300

    Added weapon damage type condition

---
+++ Assets/_Scripts/Gameplay/AbilitySystem/Conditions/WeaponDamageTypeCondition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace LA.Gameplay.AbilitySystem.Conditions
+{
+    [CreateAssetMenu(fileName = "WeaponDamageTypeCondition",
+                     menuName = "Game/Abilities/Conditions/Weapon Damage Type Condition")]
+    public class WeaponDamageTypeCondition : AbilityCondition<WeaponDamageTypeConditionParameters>
+    {
+        public override bool IsMet(BattleContext context, WeaponDamageTypeConditionParameters parameters)
+        {
+            return context.WeaponDamage.DamageType == parameters.DamageType;
+        }
+    }
+
+    [System.Serializable]
+    public class WeaponDamageTypeConditionParameters : ConditionParametersBase
+    {
+        [field: SerializeField] public DamageType DamageType { get; private set; }
+    }
+}
