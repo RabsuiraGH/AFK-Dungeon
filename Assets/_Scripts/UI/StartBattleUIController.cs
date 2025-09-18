0883479e942e8716ba92c77bb12595f66f31255c
RabsuiraGH <rabsuira@proton.me>
2025-09-18 21:40:10 +0300

    Added start battle UI

---
+++ Assets/_Scripts/UI/StartBattleUIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace LA.UI
+{
+    public class StartBattleUIController : MonoBehaviour
+    {
+        [SerializeField] private StartBattleUI _startBattleUI;
+
+        public event Action OnStartBattleRequested;
+
+
+        private void Start()
+        {
+            _startBattleUI.OnStartBattleRequested += RequestBattleStart;
+        }
+
+
+        public void Show() => _startBattleUI.Show();
+
+
+        private void RequestBattleStart()
+        {
+            OnStartBattleRequested?.Invoke();
+            _startBattleUI.Hide();
+        }
+
+
+        private void OnDestroy()
+        {
+            _startBattleUI.OnStartBattleRequested -= RequestBattleStart;
+        }
+    }
+}
