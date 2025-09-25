97a40d524dd7ec2646f3015615edc3b4933f4feb
RabsuiraGH <rabsuira@proton.me>
2025-09-26 01:59:10 +0300

    Added negative values options

--- Assets/_Scripts/UI/FillBar/FillBarWithTextUI.cs
+++ Assets/_Scripts/UI/FillBar/FillBarWithTextUI.cs
@@ -7,11 +7,15 @@ namespace LA.UI.FillBar
     {
         [SerializeField] private TextMeshProUGUI _valueText;
         [SerializeField] private string _separator = "/";
+        [SerializeField] private bool _allowsNegativeValues = false;
 
 
         public override void Fill(float currentValue, float maxValue)
         {
             base.Fill(currentValue, maxValue);
+
+            if(!_allowsNegativeValues) currentValue = 0;
+
             _valueText.text = $"{currentValue}{_separator}{maxValue}";
         }
     }
