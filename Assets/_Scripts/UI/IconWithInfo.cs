6f86fae9701c40214a3ee14e4098bf3f7fdd0ba3
RabsuiraGH <rabsuira@proton.me>
2025-10-01 16:22:17 +0300

    Added description to stats

--- Assets/_Scripts/UI/IconWithInfo.cs
+++ Assets/_Scripts/UI/IconWithInfo.cs
@@ -8,11 +8,20 @@ namespace LA.UI
     public class IconWithInfo : TemplateUI, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] public Image _iconImage;
-        public Sprite Sprite { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
+        [field: SerializeField] public Sprite Sprite { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string Description { get; private set; }
         public static event Action<IconWithInfo> OnHoverEnter;
         public static event Action<IconWithInfo> OnHoverExit;
+
+
+        private void Start()
+        {
+            if(Sprite == null && _iconImage.sprite != null)
+            {
+                Sprite = _iconImage.sprite;
+            }
+        }
 
 
         public void SetData(Sprite sprite, string name, string description)
